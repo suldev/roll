899f7e4f4f63742c96bda72723edf1a88e414134
suldev <suldev@github.com>
2020-07-27 20:48:17 -0400

    added argument check

--- src/roll.c
+++ src/roll.c
@@ -17,6 +17,9 @@ void help(int error)
 	case 3:
 		printf("Roll was malformed\n");
 		break;
+	case 4:
+		printf("Terminal DnD roller version 0.1\n");
+		return;
 	default:
 		break;
 	}
@@ -28,13 +31,42 @@ void help(int error)
 	printf("\troll 1d10-1: rolls a d10, subtracts 1 from the result and returns the value\n");
 }
 
-void parse(char* roll)
+int chkstr(char* roll)
 {
+	//Check if the roll command is malformed
+	char* c = roll;
+	int d = 0;
+	int sign = 0;
+	while(*c != 0)
+	{
+		int i = (int) *c;
+		if(i < 48 || i > 57)
+			if(i != 100 && i != 43 && i != 45)
+				return 1;
+		if(i == 100)
+			d = d + 1;
+		if(i == 43 || i == 45)
+			sign = sign + 1;
+		c = c + 1;
+	}
+	//Check if 'd' or sign is used more than once
+	if(d > 1 || sign > 1)
+		return 1;
+	return 0;
+}
+
+int parse(char* roll)
+{
+	//Parse roll argument for number of die, faces of die, and modifier
 	int i = 0;
 	int number, die;
 	int sign = 1;
 	int mod = 0;
 	char* c;
+	if(chkstr(roll) > 0)
+		return 1;
+	if(strpbrk(roll, "-"))
+		sign = -1;
 	c = strtok(roll, "d+-");
 	while(c != NULL)
 	{
@@ -50,39 +82,54 @@ void parse(char* roll)
 			mod = atoi(c);
 			break;
 		default:
-			help(3);
-			return;
+			return 1;
 		}
 		c = strtok (NULL,"d+-");
 		i = i + 1;
 	}
+	
+	if(number == 0 || die == 0)
+		return 1;
+
 	int value = 0;
 	srand((unsigned int)time(NULL));
-	if(strpbrk(roll, "-"))
-		sign = -1;
 	for(int j = 0; j < number; j = j + 1)
 	{
 		int temp = rand() % die + 1;
 		value = value + temp;
 	}
-	printf("%i\n", value + mod * sign);
+
+	if(sign < 0)
+		printf("(%i-%i)=%i\n", value, mod, value - mod);
+	else
+		printf("(%i+%i)=%i\n", value, mod, value + mod);
+	return 0;
 }
 
 int main(int argc, char *argv[])
 {
 	if(argc > 1 && (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0))
 		help(0);
+	if(argc > 1 && (strcmp(argv[1], "--version") == 0 || strcmp(argv[1], "-V") == 0))
+	{
+		help(4);
+		return 0;
+	}
 	switch(argc)
 	{
 	case 1:
 		help(1);
 		break;
 	case 2:
-		parse(argv[1]);
+		if(parse(argv[1]))
+		{
+			help(3);
+			return 1;
+		}
 		break;
 	default:
 		help(2);
-		break;
-	}		
+		return 1;
+	}
 }
 
